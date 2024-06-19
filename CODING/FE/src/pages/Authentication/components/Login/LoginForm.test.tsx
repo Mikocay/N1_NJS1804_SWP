49f6e060832149b50057e06f3706f8776235b0e1
baoha7204 <baoha3604@gmail.com>
2024-06-19 08:31:51 +0700

    [BaoHMQ] finish auth test

--- CODING/FE/src/pages/Authentication/components/Login/LoginForm.test.tsx
+++ CODING/FE/src/pages/Authentication/components/Login/LoginForm.test.tsx
@@ -1,15 +1,60 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import LoginForm from "./LoginForm";
+import authApi from "@/utils/api/authApi";
 
+const mockAuthApi = Object.assign({}, authApi);
 describe("Login Form", () => {
   beforeEach(() => {
+    mockAuthApi.login = jest.fn(() =>
+      Promise.resolve({
+        success: true,
+        data: {
+          accessToken: "accessToken",
+          refreshToken: "refreshToken",
+        },
+      })
+    );
     render(
       <BrowserRouter>
-        <LoginForm />
+        <LoginForm auth={mockAuthApi.login} />
       </BrowserRouter>
     );
+  });
+  it("should submit when enter valid credentials (Email)", async () => {
+    // Arrange
+    const { getByTestId } = screen;
+    const emailInput = "abc@gmail.com";
+    const passInput = "password";
+
+    // Act
+    await userEvent.type(getByTestId("user"), emailInput);
+    await userEvent.type(getByTestId("password"), passInput);
+    fireEvent.click(getByTestId("login"));
+    // Assert
+    await waitFor(() => expect(screen.queryAllByRole("alert")).toHaveLength(0));
+    expect(mockAuthApi.login).toHaveBeenCalledWith({
+      email: emailInput,
+      password: passInput,
+    });
+  });
+  it("should submit when enter valid credentials (Phone number)", async () => {
+    // Arrange
+    const { getByTestId } = screen;
+    const phoneInput = "0762123411";
+    const passInput = "password";
+
+    // Act
+    await userEvent.type(getByTestId("user"), phoneInput);
+    await userEvent.type(getByTestId("password"), passInput);
+    fireEvent.click(getByTestId("login"));
+    // Assert
+    await waitFor(() => expect(screen.queryAllByRole("alert")).toHaveLength(0));
+    expect(mockAuthApi.login).toHaveBeenCalledWith({
+      phoneNumber: phoneInput,
+      password: passInput,
+    });
   });
   it("should display required error when value is blank", async () => {
     // Arrange
@@ -22,15 +67,11 @@ describe("Login Form", () => {
     // Assert
     expect(userErrorMsg).toBeInTheDocument();
     expect(passErrorMsg).toBeInTheDocument();
+    expect(mockAuthApi.login).not.toHaveBeenCalled();
   });
   it.each([
-    { input: "test", expected: "Invalid email/phone number" },
-    { input: "test@", expected: "Invalid email/phone number" },
-    { input: "test@test", expected: "Invalid email/phone number" },
-    { input: "test@test.", expected: "Invalid email/phone number" },
     { input: "test@test.c", expected: "Invalid email/phone number" },
     { input: "0762abc", expected: "Invalid email/phone number" },
-    { input: "076212322", expected: "Invalid email/phone number" },
     { input: "07621234111", expected: "Invalid email/phone number" },
   ])(
     "should display matching error when enter $input to email/phone number field",
@@ -42,12 +83,13 @@ describe("Login Form", () => {
       // Act
       await userEvent.type(getByTestId("user"), input);
       await userEvent.type(getByTestId("password"), passInput);
-      fireEvent.click(getByTestId("login"), { name: /Sign in/i });
+      fireEvent.click(getByTestId("login"));
       const userErrorMsg = await findByText(expected);
       // Assert
       expect(userErrorMsg).toBeInTheDocument();
       expect(getByTestId("user")).toHaveValue(input);
       expect(getByTestId("password")).toHaveValue(passInput);
+      expect(mockAuthApi.login).not.toHaveBeenCalled();
     }
   );
 });
--- CODING/FE/src/pages/Authentication/components/Register/RegisterForm.test.tsx
+++ CODING/FE/src/pages/Authentication/components/Register/RegisterForm.test.tsx
@@ -1,14 +1,39 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import RegisterForm from "./RegisterForm";
+import authApi from "@/utils/api/authApi";
+import userEvent from "@testing-library/user-event";
 
+const mockAuthApi = Object.assign({}, authApi);
 describe("Register Form", () => {
   beforeEach(() => {
+    mockAuthApi.register = jest.fn(() => Promise.resolve({ success: true }));
     render(
       <BrowserRouter>
-        <RegisterForm />
+        <RegisterForm auth={mockAuthApi.register} />
       </BrowserRouter>
     );
+  });
+  it("should submit when enter valid credentials (Email)", async () => {
+    // Arrange
+    const { getByTestId } = screen;
+    const emailInput = "abc@gmail.com";
+    const phoneInput = "0762123411";
+    const passInput = "password";
+
+    // Act
+    await userEvent.type(getByTestId("email"), emailInput);
+    await userEvent.type(getByTestId("phoneNumber"), phoneInput);
+    await userEvent.type(getByTestId("password"), passInput);
+    await userEvent.type(getByTestId("confirmPassword"), passInput);
+    fireEvent.click(getByTestId("register"));
+    // Assert
+    await waitFor(() => expect(screen.queryAllByRole("alert")).toHaveLength(0));
+    expect(mockAuthApi.register).toHaveBeenCalledWith({
+      email: emailInput,
+      phoneNumber: phoneInput,
+      password: passInput,
+    });
   });
   it("should display required error when value is blank", async () => {
     // Arrange
@@ -27,5 +52,47 @@ describe("Register Form", () => {
     expect(phoneErrorMsg).toBeInTheDocument();
     expect(passErrorMsg).toBeInTheDocument();
     expect(confirmPassErrorMsg).toBeInTheDocument();
+    expect(mockAuthApi.register).not.toHaveBeenCalled();
   });
+  it.each([
+    {
+      input: {
+        email: "test@test.c",
+        phoneNumber: "0762abc",
+        password: "pass",
+        confirmPassword: "pass1",
+      },
+      expected: [
+        "Invalid email address",
+        "Invalid phone number",
+        "Passwords do not match",
+      ],
+    },
+  ])(
+    "should display matching error when enter invalid input ",
+    async ({ input, expected }) => {
+      // Arrange
+      const { getByTestId, findByText } = screen;
+
+      // Act
+      await userEvent.type(getByTestId("email"), input.email);
+      await userEvent.type(getByTestId("phoneNumber"), input.phoneNumber);
+      await userEvent.type(getByTestId("password"), input.password);
+      await userEvent.type(
+        getByTestId("confirmPassword"),
+        input.confirmPassword
+      );
+      fireEvent.click(getByTestId("register"));
+      const errorMessages = expected.map((msg) => findByText(msg));
+      // Assert
+      for (const errorMsg of errorMessages) {
+        expect(await errorMsg).toBeInTheDocument();
+      }
+      expect(getByTestId("email")).toHaveValue(input.email);
+      expect(getByTestId("phoneNumber")).toHaveValue(input.phoneNumber);
+      expect(getByTestId("password")).toHaveValue(input.password);
+      expect(getByTestId("confirmPassword")).toHaveValue(input.confirmPassword);
+      expect(mockAuthApi.register).not.toHaveBeenCalled();
+    }
+  );
 });

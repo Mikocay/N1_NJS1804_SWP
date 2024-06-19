453e41b0806ce41c68f931ef46ea906dfa330345
Mikocay <huynhtd97@gmail.com>
2024-06-19 15:52:11 +0700

    [DuongHT] test create and view staff

---
+++ CODING/FE/src/pages/Admin/adminViewList/AllStaffList/AllStaffList.test.tsx
@@ -0,0 +1,34 @@
+// Mock post function
+import { BrowserRouter } from "react-router-dom";
+import AdminAllStaffList from ".";
+import { render, waitFor } from "@testing-library/react";
+import { API_ENDPOINTS } from "@/utils/api";
+import { get } from "@/utils/apiCaller";
+
+jest.mock('@/utils/apiCaller', () => ({
+    get: jest.fn(),
+}));
+
+describe('All Dentist List', () => {
+    beforeEach(() => {
+        render(
+            <BrowserRouter>
+                <AdminAllStaffList />
+            </BrowserRouter>
+        );
+    });
+
+    it('should display all staffs', async () => {
+        // Mock get API response
+        (get as jest.Mock).mockResolvedValue({
+            success: true,
+            data: { list: [] }, // Mock data as needed
+        });
+
+        // Asserte
+        await waitFor(() => {
+            expect(get).toHaveBeenCalledWith(API_ENDPOINTS.USERS.LIST_DENTIST);
+        })
+
+    })
+})
